6ab31c6dbf77e23054355ec786c7a90230515323
kiichiro <kiichirou.k@yahoo.ne.jp>
2021-05-26 22:39:44 +0900

    counter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <counter/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Counter from './components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
+
+
+
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="counter">
+        <p class="cnt">Counter</p>
+        <div class="number">{{num}}</div>
+        <button class="plus" @click="addOne">＋</button>
+        <button class="minus" @click="substractOne">－</button>
+        <p ><button class="reset" @click="reset">reset</button></p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Counter",
+        data(){
+            return{
+                num:0
+            }
+        },
+        methods: {
+            addOne: function(){
+                this.num = this.num +1;
+            },
+            substractOne: function(){
+                this.num = this.num -1;
+            },
+            reset: function(){
+                this.num = 0;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+.cnt{
+    font-size:30px;
+}
+
+button{
+    display:inline-block;
+    font-size:50px;
+    color: white;
+}
+
+.number{
+    font-size:150px;
+    margin:10px;
+}
+
+.plus{
+    background-color: red;
+    padding:20px 50px;
+    margin: 10px;
+}
+
+.minus{
+    background-color: blue;
+    padding:20px 50px;
+}
+
+.reset{
+    background-color: black;
+    margin: 10px;
+    padding:10px 30px;
+}
+
+</style>
