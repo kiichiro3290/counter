e21af4bc9096748d23ef4b534a2a2a01c45a1192
kiichiro <kiichirou.k@yahoo.ne.jp>
2021-06-01 15:53:55 +0900

    過去10時間分を表示できる

--- src/components/GetAPI.vue
+++ src/components/GetAPI.vue
@@ -8,10 +8,18 @@
             <p class="result-value">{{resultValue}}</p>
         </div>
         <div class="table">    
-            <div class="table-line" v-for="(info, name) in infos" :key="info">
-                <p class="table-column">{{name}}</p>
-                <p>{{info}}</p>
+            <div class="table-column">
+                
+                <div  v-for="(info, num) in infos" :key="info" class="column-value">
+                    <div v-for="(i,name) in info" :key="i">
+                        <p v-if="num==0">{{name}}</p>
+                        <p>{{i}}</p>
+                    </div>
+                </div>
             </div>
+            <!-- <div class="table-line">
+                <p class="tapple-value" v-for= "info in infos" :key="info">{{info}}</p>
+            </div> -->
         </div>
     </div>
 </template>
@@ -20,9 +28,12 @@
 
 <script>
     
+    //API用のフレームワークaxios
     import axios from 'axios'
 
-    let edgeId = 0;
+    //センサーのIDを格納する変数
+    let edgeId = 0; 
+
 
     export default{
         name:"GetApi",
@@ -35,29 +46,43 @@
         },
         methods: {
             getSensorId: function(){
+                //値をリセット
                 this.infos = "";
                 this.resultValue="";
-                console.log(this.resultValue);
+
+                //センサーIDをhtmlに表示
                 const sensor = document.getElementById("sensor").value;
                 this.sensorValue = sensor;
                 edgeId = Number(sensor);
+                
+                //日付を取得
+                let DateTime = new Date();
+                console.log(Math.floor(DateTime.getTime()/1000)-1000);
+                let yesterdayTime = Math.floor(DateTime.getTime()/1000)-600;
+                let nowTime = Math.floor(DateTime.getTime()/1000);
+
+                //APIにデータを貰う
                 axios
-                .get(`https://platform.coi.kyushu-u.ac.jp/raspi/area/${edgeId}?start={unixtime}&end={unixtime}`)
-                .then(response => {
+                .get(`https://platform.coi.kyushu-u.ac.jp/raspi/area/${edgeId}?start=${yesterdayTime}&end=${nowTime}`)
+                 .then(response => {
                     console.log(response);
-                    console.log(response.data);
+                    
+                    //
                     let datas = response["data"];
                     if(datas.result=="false2")
                     {
+                        //存在しないセンサーID
                         this.resultValue="センサーIDが間違っています";
                         console.log(this.resultValue);
                     }
                     else{    
+                        //時間を文字列に変換
                         this.infos = JSON.parse(datas);
-                        console.log(datas);
-                        let dateTime = new Date(parseInt(this.infos.time));
-                        console.log(dateTime.toString());
-                        this.infos.time = dateTime;
+                        for(let i = 0; i <= this.infos.length; i++){
+                            let resultTime = new Date(parseInt(this.infos[i]["time"]));
+                            this.infos[i].time = resultTime.toString();
+                        }
+                        console.log(this.infos[0]);
                     }
                 })
             },
@@ -85,25 +110,7 @@
     font-size: 18px;
 }
 
-.table p{
-    width:14em;
-    display: inline-block;
-    
-}
 
-.table-column{
-    border-right: black 2px solid;
-}
-
-.table-line{
-    border: black 1px solid;
-    width: 30em;
-    margin: 0 auto;
-}
-
-.table{
-    font-size: 16px;
-}
 
 
 </style>
